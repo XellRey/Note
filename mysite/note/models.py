9a29c497f10cba9984b742f24343882682459263
XellRey <ivanshvediyk@gmail.com>
2023-01-04 14:31:47 +0300

    version 0.2

--- mysite/note/models.py
+++ mysite/note/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    note_title = models.CharField(max_length=30)
+    pub_date = models.DateTimeField('date create')
+    note_text = models.CharField(max_length=3000)
+
+    def __str__(self):
+        return self.note_title
